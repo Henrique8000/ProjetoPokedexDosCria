c338e4a4d5038071fc1f26f4a8cefa40782120b1
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-03-19 13:02:33 -0300

    Adicionar os arquivos do projeto

---
+++ ProjectPoke/ProjectPoke/ProjectPokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPokeApp.swift
+//  ProjectPoke
+//
+//  Created by Aluno Mack on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
