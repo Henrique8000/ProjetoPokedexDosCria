1bdfe7c6c247f96d9bb669d72aa580a0d5354cdf
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-03-20 13:12:19 -0300

    Update ContentView.swift

--- ProjectPoke/ProjectPoke/ContentView.swift
+++ ProjectPoke/ProjectPoke/ContentView.swift
@@ -1,20 +1,34 @@
 //
 //  ContentView.swift
-//  ProjectPoke
+//  pokedex3
 //
-//  Created by Aluno Mack on 19/03/25.
+//  Created by Aluno Mack on 20/03/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       PokedexView()
+        ScrollPokedex()
+        StartView()
+        
+        }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        struct ContentView_Preview: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+
+struct MoreView: View {
+    var body: some View {
+        List {
+            Text("Thing 1")
+            Text("Thing 2")
+            Text("Thing 3")
+            Text("Thing 4")
+            Text("Thing 5")
+        }
     }
 }
