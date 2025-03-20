76b1a371c53283fc448e4b0172e1703e662f27c2
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-03-20 13:13:07 -0300

    Update ScrollPokedex.swift

--- ProjectPoke/ProjectPoke/ScrollPokedex.swift
+++ ProjectPoke/ProjectPoke/ScrollPokedex.swift
@@ -1,34 +1,46 @@
+//
+//  ScrollPokedex.swift
+//  pokedex3
+//
+//  Created by Aluno Mack on 20/03/25.
+//
+
 import SwiftUI
 
 struct ScrollPokedex: View {
     var body: some View {
-
         ScrollView{
-
                 VStack (spacing: 20) {
-                    Image("charizard")
+                    ForEach(pokemons, id: \.self) { pokemon in
+                        Image(pokemon.name)
                             .resizable()
                             .frame(width: 100, height: 100)
-                    Text("Charizard")
+                        Text(pokemon.name)
                             .font(.largeTitle)
-                    
-                    Image("Daco_4278178")
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                    
-                    Text("Venusaur")
-                        .font(.largeTitle)
-                    
-                    Image("Daco_4379083")
-                        .resizable()
-                        .frame(width: 150, height: 100)
-                    Text("Zapdos")
-                        .font(.largeTitle)
-                       
+                    }
+//                    Image("charizard")
+//                            .resizable()
+//                            .frame(width: 100, height: 100)
+//                    Text("Charizard")
+//                            .font(.largeTitle)
+//
+//                    Image("zapdos")
+//                        .resizable()
+//                        .frame(width: 100, height: 100)
+//
+//                    Text("Venusaur")
+//                        .font(.largeTitle)
+//
+//                    Image("venusaur")
+//                        .resizable()
+//                        .frame(width: 150, height: 100)
+//                    Text("Zapdos")
+//                        .font(.largeTitle)
+                        
                 
                     }
+                }
             }
-    }
 }
 
 struct ScrollPokedex_Previews: PreviewProvider {
